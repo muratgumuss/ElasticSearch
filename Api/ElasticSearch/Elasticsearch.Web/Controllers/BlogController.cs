7c978fb36d3dd222a49388f47ee7fe023f19ed7f
Murat Gümüş <muratgumus05@gmail.com>
2026-03-02 17:58:25 +0300

    elasticsearch blog app save & search

---
+++ Api/ElasticSearch/Elasticsearch.Web/Controllers/BlogController.cs
@@ -0,0 +1,48 @@
+﻿using Elasticsearch.Web.Services;
+using Elasticsearch.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elasticsearch.Web.Controllers
+{
+    public class BlogController : Controller
+    {
+        private BlogService _blogService;
+
+        public BlogController(BlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public async Task<IActionResult> Search()
+        {
+            return View(await _blogService.SearchAsync(string.Empty));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Search(string searchText)
+        {
+            ViewBag.searchText = searchText;
+
+            return View(await _blogService.SearchAsync(searchText));
+        }
+
+        public IActionResult Save()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(BlogCreateViewModel model)
+        {
+            var isSucces = await _blogService.SaveAsync(model);
+
+            if (!isSucces)
+            {
+                TempData["result"] = "kayıt başarısız";
+                return RedirectToAction(nameof(BlogController.Save));
+            }
+
+            TempData["result"] = "kayıt başarılı";
+            return RedirectToAction(nameof(BlogController.Save));
+        }
+    }
+}
---
+++ Api/ElasticSearch/Elasticsearch.Web/ViewModels/BlogCreateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Elasticsearch.Web.ViewModels
+{
+    public class BlogCreateViewModel
+    {
+
+        [Display(Name = "Blog Title")]
+        [Required]
+        public string Title { get; set; } = null!;
+
+        [Display(Name = "Blog Content")]
+        [Required]
+        public string Content { get; set; } = null!;
+
+        [Display(Name = "Blog Tags")]
+        public string Tags { get; set; } = null!;
+    }
+}
